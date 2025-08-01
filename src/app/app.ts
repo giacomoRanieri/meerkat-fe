fa1a7f46b54e4e8f2d37d3d33a3af037ec431921
LoredanaITS <lory.frontino@gmail.com>
2025-08-01 23:16:47 +0200

    added fonts,favicon, header and footer component blank, hompage blank

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './components/header/header.component';
+import {FooterComponent} from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [RouterOutlet,HeaderComponent, FooterComponent],
+  templateUrl: './app.html'
 })
 export class App {
   protected readonly title = signal('devfestSite-FE');
